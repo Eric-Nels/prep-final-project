fc8fdb63f07dea40ccffdb1172002586ebbe8c9b
Eric Nelson <ernelson12o0@gmail.com>
2023-02-06 20:08:28 -0800

    set title, added images, first attempt at adding event

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "A History";
 document.querySelector("body").appendChild(h2);
+
+const rav4 = document.getElementById("rav4");
+rav4.addEventListener("click", function() {
+   document.getElementById("rav4Paragraph").style.display = "block";
+});
